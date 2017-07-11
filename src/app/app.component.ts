3d3b155765ad7f2cca76f2796a71a2c8cb8a3bdc
pastorsj <pastorsj@rose-hulman.edu>
2017-07-11 18:22:42 -0400

    Updated to angular cli, angular 4

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'Sam Pastoriza'
+  work = 'Web design taken to the next level.'
+  description = 'A rising senior at Rose-Hulman Institute of Technology with interests in website development, photography and golf'
+}
