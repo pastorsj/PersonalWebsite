7d6618e49c684ef3d3b71fd084c331562b746c43
pastorsj <pastorsj@rose-hulman.edu>
2017-09-08 19:08:55 -0400

    Updated personal website using https server

--- server.js
+++ server.js
@@ -2,10 +2,31 @@
 Simple static file server that will send index.html if the file is not found. Useful for developing angular apps.
 */ 
 
-var express = require("express");
-var app = express();
+/**
+ * Module dependencies.
+ */
 
-var PORT = 3000;
+import http from 'http';
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
+import express from 'express';
+
+const app = express();
+
+const privateKey = fs.readFileSync(path.join(__dirname, '/sslcert/key.pem'), 'utf8');
+const certificate = fs.readFileSync(path.join(__dirname, '/sslcert/certificate.pem'), 'utf8');
+const credentials = {key: privateKey, cert: certificate};
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+const port = normalizePort(process.env.PORT || '3000');
+const httpsPort = normalizePort(process.env.PORT || '3001');
+
+app.set('port', port);
+app.set('httpsPort', httpsPort);
 
 app.use(express.static("dist"));
 
@@ -13,7 +34,82 @@ app.use((req, res) => {
     res.sendFile(__dirname + "/dist/index.html");
 })
 
-app.listen(PORT, function () {
-  console.log("Angular App Is Running on port " + PORT + "!");
-  console.log("Open http://localhost:" + PORT + "/ on your browser!");
-});
+/**
+ * Create HTTP/S server.
+ */
+
+const server = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+httpsServer.listen(httpsPort);
+httpsServer.on('error', onError);
+httpsServer.on('listening', onListeningHttps);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ * @param {Object} error - The object containing the error
+ */
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+    console.log('Http server listening on ' + bind);
+}
+
+function onListeningHttps() {
+    var addr = httpsServer.address();
+    var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+    console.log('Https server listening on ' + bind);
+}
