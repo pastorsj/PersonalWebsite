7a872f43333efbfe55c500cfe1363c821408e05a
pastorsj <pastorsj@rose-hulman.edu>
2017-10-07 11:35:54 -0400

    added back restart.sh

--- scripts/deploy.sh
+++
@@ -1,3 +0,0 @@
-scp -o "StrictHostKeyChecking no" -r $TRAVIS_BUILD_DIR/dist $USER@$HOST:/home/ubuntu
-scp -o "StrictHostKeyChecking no" $TRAVIS_BUILD_DIR/server.dist.js $USER@$HOST:/home/ubuntu
-ssh $USER@$HOST:/home/ubuntu 'bash -s' < ./restart.sh
