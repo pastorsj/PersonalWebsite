32de1132595be7501605e7e156c1d27dddbbd8fc
pastorsj <pastorsj@rose-hulman.edu>
2018-01-21 14:21:06 -0500

    Updated packages and service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { environment } from 'environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production})
   ],
   providers: [],
   bootstrap: [AppComponent]
